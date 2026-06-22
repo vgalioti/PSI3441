e0c1df7c56649d7c198e3290f5fd57b21940aa88
vgalioti <vgbarbosa@usp.br>
2026-06-22 13:03:55 -0300

    Working ATV7_P3

---
+++ ATIVIDADE7_P3/src/main.c
@@ -0,0 +1,73 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <stdlib.h>
+
+#define PADEIRO_STACK_SIZE  1024
+#define CLIENTE_STACK_SIZE  1024
+#define PADEIRO_PRIORITY    5
+#define CLIENTE_PRIORITY    3
+#define VITRINE_CAPACITY    10
+
+K_THREAD_STACK_DEFINE(padeiro, PADEIRO_STACK_SIZE);
+K_THREAD_STACK_DEFINE(cliente, CLIENTE_STACK_SIZE);
+static struct k_thread padeiro_thread_data;
+static struct k_thread cliente_thread_data;
+
+volatile int saldo_vitrine = 0;
+
+K_MUTEX_DEFINE(mutex_vitrine);
+K_SEM_DEFINE(sem_paes, 0, VITRINE_CAPACITY);
+K_SEM_DEFINE(sem_espaco, VITRINE_CAPACITY, VITRINE_CAPACITY);
+
+void padeiro_thread(void *arg1, void *arg2, void *arg3)
+{
+    while (1) {
+        k_sleep(K_MSEC(1500));
+
+        k_sem_take(&sem_espaco, K_FOREVER);
+
+        k_mutex_lock(&mutex_vitrine, K_FOREVER);
+        saldo_vitrine++;
+        printk("Padeiro: Pão pronto!\n");
+        printk("Saldo da vitrine: %d\n", saldo_vitrine);
+        printk("=====================================\n");
+        k_mutex_unlock(&mutex_vitrine);
+
+        k_sem_give(&sem_paes);
+    }
+}
+
+void cliente_thread(void *arg1, void *arg2, void *arg3)
+{
+    while (1) {
+        k_sleep(K_MSEC(1500));
+        
+        k_sem_take(&sem_paes, K_FOREVER);
+        
+        k_mutex_lock(&mutex_vitrine, K_FOREVER);
+        saldo_vitrine--;
+        printk("Cliente: Comprei pão!\n");
+        printk("Saldo da vitrine: %d\n", saldo_vitrine);
+        printk("=====================================\n");
+        k_mutex_unlock(&mutex_vitrine);
+
+        k_sem_give(&sem_espaco);
+    }
+}
+
+void main(void)
+{
+    printk("\n========== Atividade 7 - Parte 3 ==========\n");
+
+    k_thread_create(&cliente_thread_data, cliente, CLIENTE_STACK_SIZE,
+                    cliente_thread, NULL, NULL, NULL,
+                    CLIENTE_PRIORITY, 0, K_NO_WAIT);
+
+    k_thread_create(&padeiro_thread_data, padeiro, PADEIRO_STACK_SIZE,
+                    padeiro_thread, NULL, NULL, NULL,
+                    PADEIRO_PRIORITY, 0, K_NO_WAIT);
+
+    while (1) {
+        k_sleep(K_FOREVER);
+    }
+}
